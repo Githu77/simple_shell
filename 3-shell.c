c8c9ecf8f3c973b60c02bc41b53f0b4e1ff1eebe
Githu77 <githubenard16@gmail.com>
2023-05-25 03:57:11 +0300

    add

--- 3-shell.c
+++ 3-shell.c
@@ -26,6 +26,86 @@ int _strcmp(char *str1, char *str2)
 	return (str1[x] - str2[x]);
 }
 
+/**
+*_strcat - joins two strings
+*@dest: string to join to
+*@src: string to add
+*Return: pointer to new string
+*
+*
+*
+*
+*
+*
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *n_string =  NULL;
+	int l_dest = _strlen(dest);
+	int l_source = _strlen(src);
+
+	n_string = malloc(sizeof(*n_string) * (l_dest + l_source + 1));
+	_strcpy(dest, n_string);
+	_strcpy(src, n_string + l_dest);
+	n_string[l_dest + l_source] = '\0';
+	return (n_string);
+}
+
+/**
+*_strspn - length of first string
+*@str: string to check
+*@accept: string to use
+*Return: bytes numbers
+*
+*
+*
+*
+*
+*/
+
+int _strspn(char *str1, char *str2)
+{
+	int x = 0;
+	int c = 0;
+
+	while (str1[x] != '\0')
+	{
+		if (_strchr(str2, str1[x]) == NULL)
+			break;
+		c++;
+		x++;
+	}
+	return (c);
+}
+
+/**
+*_strcspn - checks unique characters in str1
+*@str1: string to be searched
+*@str2: string to be used
+*Return: index at which a char in str1 exists in str2
+*
+*
+*
+*
+*
+ */
+
+
+
+int _strcspn(char *str1, char *str2)
+{
+	int l = 0, x;
+
+	for (x = 0; str1[x] != '\0'; x++)
+	{
+		if (_strchr(str2, str1[x]) != NULL)
+			break;
+		l++;
+	}
+	return (l);
+}
+
+
 
 
 
--- 5-shell.c
+++ 5-shell.c
@@ -1,40 +1,5 @@
 #include "shell.h"
 
-
-void handle_no_input(void)
-{
-	char **cc = NULL;
-	int i, tc = 0;
-	size_t n = 0;
-
-	if (!(isatty(STDIN_FILENO)))
-	{
-		while (getline(&user_inp, &n, stdin) != -1)
-		{
-			delete_nl(user_inp);
-			delete_comm(user_inp);
-			inp_data = create_tokens(user_inp, ";");
-			for (i = 0; inp_data[i] != NULL; i++)
-			{
-				cc = create_tokens(inp_data[i], " ");
-				if (cc[0] == NULL)
-				{
-					free(cc);
-					break;
-				}
-				tc = identify_inp(cc[0]);
-				start(cc, tc);
-				free(cc);
-			}
-			free(inp_data);
-		}
-		free(user_inp);
-		exit(state);
-	}
-}
-
-
-
 /**
 * initializer - starts shell
 * @current_command: current token
